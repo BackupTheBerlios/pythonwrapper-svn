b836bcc773f807b4d8138583af5072c25198887d
cculver <cculver@827b5c2f-c6f0-0310-96fe-c05a662c181e>
2005-02-23 21:32:07 +0000

    Inital checkin of files.

---
+++ PythonWrapper/include/Common.h
@@ -0,0 +1,6 @@
+#ifndef _Common_h_
+#define _Common_h_
+
+#define DLL_EXPORT __declspec( dllexport )
+
+#endif
---
+++ PythonWrapper/source/PythonWrapper.cpp
@@ -0,0 +1,65 @@
+#include "PythonWrapper.h"
+#include <boost/python.hpp>
+
+namespace python = boost::python;
+
+python::object *PythonWrapper::sModule = 0;
+unsigned int PythonWrapper::sInstanceCount = 0;
+
+PythonWrapper::PythonWrapper( )
+{
+    if ( sInstanceCount++ == 0 )
+    {
+        Py_Initialize();
+
+        assert( sModule == 0 );
+        sModule = new python::object(python::handle<>(python::borrowed(PyImport_AddModule("__main__"))));
+    }
+
+    init( );
+} // PythonWrapper
+
+
+PythonWrapper::~PythonWrapper( )
+{
+    if ( --sInstanceCount == 0 )
+    {
+        assert( sModule != 0 );
+        delete sModule;
+        sModule = 0;
+
+        Py_Finalize();
+    }
+
+    deinit( );
+} // ~PythonWrapper
+
+
+void PythonWrapper::init( )
+{
+    // This is a workaround.  The dict constructor does not make a deep copy
+    // as it is supposed to.  This ensures that we retrieve a copy of the dict,
+    // not the original.
+    mNamespace = python::dict(sModule->attr("__dict__")).copy();
+} // init
+
+
+void PythonWrapper::deinit()
+{
+    // Clear namespace so we don't have a reference to it.
+    mNamespace = python::dict( );
+} // deinit
+
+
+void PythonWrapper::runString( const char *str )
+{
+    try
+    {
+        handle<> result(PyRun_String(str, Py_file_input, mNamespace.ptr(), mNamespace.ptr()));
+    }
+    catch (...) // todo: use the correct error catch
+    {
+        // todo: Throw exception on error, query for error message/type.
+        PyErr_Clear( );
+    }
+}
